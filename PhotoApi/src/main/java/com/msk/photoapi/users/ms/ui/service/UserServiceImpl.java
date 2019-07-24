11ef9fc240224670565ad661b7148eaa842b292b
mandar.skulkarni <mandar.skulkarni@P5LAP-G01L10X.dir.svc.accenture.com>
2019-07-24 12:37:06 +0530

    Added security for services. Password encryption is done on service layer.

--- PhotoApi/src/main/java/com/msk/photoapi/users/ms/ui/service/UserServiceImpl.java
+++ PhotoApi/src/main/java/com/msk/photoapi/users/ms/ui/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.msk.photoapi.users.ms.database.UserEntity;
@@ -15,19 +16,22 @@ import com.msk.photoapi.users.ms.ui.model.shared.UserDto;
 public class UserServiceImpl implements UsersServiceI {
 	@Autowired
 	UsersRepository usersRepository;
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public UserServiceImpl(UsersRepository usersRepository) {
+	public UserServiceImpl(UsersRepository usersRepository,BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.usersRepository = usersRepository;
+		this.bCryptPasswordEncoder=bCryptPasswordEncoder;
 
 	}
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPwd(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = mapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPwd("Tes1");
 		usersRepository.save(userEntity);
 		UserDto returnValue = mapper.map(userEntity, UserDto.class);
 		return returnValue;
